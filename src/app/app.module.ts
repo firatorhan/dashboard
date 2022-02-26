dd3b934bc09647162c470cd3911153bea1f52bba
firatorhan <f-ratorhan@hotmail.com>
2022-02-26 16:02:45 +0300

    Feat: add big natification widget

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { SubmenuComponent } from './submenu/submenu.component';
 import { TopMenuComponent } from './top-menu/top-menu.component';
+import { PinkChartComponent } from './pink-chart/pink-chart.component';
+import { NgApexchartsModule } from 'ng-apexcharts';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     SubmenuComponent,
-    TopMenuComponent
+    TopMenuComponent,
+    PinkChartComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgApexchartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pink-chart/pink-chart.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Chart } from 'chart.js';
+
+@Component({
+  selector: 'app-pink-chart',
+  templateUrl: './pink-chart.component.html',
+  styleUrls: ['./pink-chart.component.css']
+})
+export class PinkChartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+ 
+  
+   }
+ 
+  }
+  
+
